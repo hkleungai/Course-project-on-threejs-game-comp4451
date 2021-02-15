743c8aede3e881c340ccf9e73952778a6bc0fa11
hkleungai <43258070+hkleungai@users.noreply.github.com>
2021-02-16 04:30:25 +0800

    feat: Add zig-zag layout on main

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,4 +3,7 @@
 const randint = upper_bound => Math.floor(Math.random() * upper_bound);
 const range = upper_bound => [...Array(upper_bound).keys()];
 
-export { randint, range };
+const sin_deg = deg => Math.sin(deg / 180 * Math.PI);
+const cos_deg = deg => Math.cos(deg / 180 * Math.PI);
+
+export { randint, range, sin_deg, cos_deg };
